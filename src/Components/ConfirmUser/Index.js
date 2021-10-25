596affc96b66bf50b179ebc21b4ef49e72c033df
mohamedelbalshy <mohamedbalshy2014@gmail.com>
2021-10-26 01:34:32 +0200

    fixes in navbar

--- src/Components/ConfirmUser/Index.js
+++ src/Components/ConfirmUser/Index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./Hero";
+import Navbar from "./navbar";
 
 export default function Index() {
   return (
     <div>
+      <Navbar />
       <Hero />
     </div>
   );
